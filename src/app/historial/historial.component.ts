a213e555187055eb94727d3d5ca0bb7a78184483
estela801 <ros98cg98@gmail.com>
2020-08-28 23:20:07 -0600

    Estilo y parte del TS

--- src/app/historial/historial.component.ts
+++ src/app/historial/historial.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./historial.component.css']
 })
 export class HistorialComponent implements OnInit {
+  historial=[];
+  consulta:any;
+  nombre:string;
+  descripcion:string;
+  mascota:string;
+  veterinario:string;
+  visita:number;
+  precio:number;
+  error:number;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  ingresar()
+  {}
 
 }
