a77c09e43c16e838366b28d030054461a452e533
estela801 <ros98cg98@gmail.com>
2020-08-29 00:17:19 -0600

    Codigo TS, cálculos funcionando y se muestran en tabla

--- src/app/historial/historial.component.ts
+++ src/app/historial/historial.component.ts
@@ -15,12 +15,55 @@ export class HistorialComponent implements OnInit {
   visita:number;
   precio:number;
   error:number;
+  contador:number;
+  descuento:number;
+  ttpagar:number;
+  
 
   constructor() { }
 
   ngOnInit(): void {
+    this.nombre='';
+    this.mascota='';
+    this.veterinario='';
+    this.descripcion='';
+    this.visita=0;
+    this.precio=0;
+    this.error=0;
+    this.contador=0;
+
   }
   ingresar()
-  {}
+  {
+    if(this.precio>0 && this.nombre!='' && this.descripcion!='' && this.mascota!='' && this.veterinario!=''&& this.visita>0)
+    {
+      this.error=0;
+        if(this.visita<2)
+        {
+          this.ttpagar=this.precio;
+
+        } else if(this.visita<=4)
+        {
+          this.descuento=this.precio*0.05;
+          this.ttpagar=this.precio-this.descuento;
+        }
+
+        else if(this.visita>4)
+        {
+          this.descuento=this.precio*0.10;
+          this.ttpagar=this.precio-this.descuento;
+        }
+
+  this.consulta={"nombre":this.nombre,"mascota": this.mascota,"veterinario":this.veterinario,"descripcion":this.descripcion,"visita":this.visita,"precio":this.precio,"ttpagar":this.ttpagar};
+  this.historial.push(this.consulta);
+  this.contador++;
+
+
+    }
+    else
+    {
+      this.error=1;
+    }
+  }
 
 }
