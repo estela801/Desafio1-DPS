d2c86523a9c4035e6f3560e1b80b1ce21f615dc8
kfuentes466 <kfuentes466@gmail.com>
2020-08-29 18:45:07 -0600

    Ya cuenta las visitas, pero ttpagar no se muestra, ayuda

--- src/app/historial/historial.component.ts
+++ src/app/historial/historial.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-historial',
   templateUrl: './historial.component.html',
@@ -29,7 +29,7 @@ export class HistorialComponent implements OnInit {
     this.mascota='';
     this.veterinario='';
     this.descripcion='';
-    this.visita=0;
+    //this.visita=0;
     this.precio=0;
     this.error=0;
     this.contador=0;
@@ -40,11 +40,11 @@ export class HistorialComponent implements OnInit {
   }
   ingresar()
   {
-    if(this.precio>0 && this.nombre!='' && this.descripcion!='' && this.mascota!='' && this.veterinario!='' && this.visita>0 && this.dui!='' && this.medicamento!='')
+    if(this.precio>0 && this.nombre!='' && this.descripcion!='' && this.mascota!='' && this.veterinario!='' /*&& this.visita>0*/ && this.dui!='' && this.medicamento!='')
     {
       this.error=0;
         
-      if(this.visita == 2)
+      /*if(this.visita == 2)
         {
           this.descuento=this.precio*0.05;
           this.ttpagar=this.precio-this.descuento;
@@ -61,8 +61,10 @@ export class HistorialComponent implements OnInit {
 
         }
 
-  this.consulta={"dui":this.dui,"medicamento":this.medicamento,"nombre":this.nombre,"mascota": this.mascota,"veterinario":this.veterinario,"descripcion":this.descripcion,"visita":this.visita,"precio":this.precio,"ttpagar":this.ttpagar};
+  this.consulta={"dui":this.dui,"medicamento":this.medicamento,"nombre":this.nombre,"mascota": this.mascota,"veterinario":this.veterinario,"descripcion":this.descripcion,"visita":this.visita,"precio":this.precio,"ttpagar":this.ttpagar};*/
+  this.onVisita();
   this.historial.push(this.consulta);
+  console.log(this.historial);
   this.contador++;
 
 
@@ -72,5 +74,38 @@ export class HistorialComponent implements OnInit {
       this.error=1;
     }
   }
+
+  onVisita(){
+    let x: number;
+    let consul:number;
+
+    if(this.historial.length < 1){
+      this.consulta={"dui":this.dui,"medicamento":this.medicamento,"nombre":this.nombre,"mascota": this.mascota,"veterinario":this.veterinario,"descripcion":this.descripcion,"visita":1,"precio":this.precio,"ttpagar":this.ttpagar};
+    }else{
+      for(x=0;x<this.historial.length;x++){
+        if(this.historial[x].dui==this.dui){
+          console.log("Si se repiten");
+          consul = this.historial[x].visita++;
+          
+          if(consul == 2)
+        {
+          this.descuento=this.precio*0.05;
+          this.ttpagar=this.precio-this.descuento;
+        }
+
+        else if(consul>4)
+        {
+          this.descuento=this.precio*0.10;
+          this.ttpagar=this.precio-this.descuento;
+        }
+        this.consulta={"dui":this.dui,"medicamento":this.medicamento,"nombre":this.nombre,"mascota": this.mascota,"veterinario":this.veterinario,"descripcion":this.descripcion,"visita":consul,"precio":this.precio,"ttpagar":this.ttpagar};
+        }else{
+          console.log("No se repite");
+          this.ttpagar=this.precio;
+          this.consulta={"dui":this.dui,"medicamento":this.medicamento,"nombre":this.nombre,"mascota": this.mascota,"veterinario":this.veterinario,"descripcion":this.descripcion,"visita":1,"precio":this.precio,"ttpagar":this.precio};
+        }
+    }
+  }
+}
 
 }
